5e1f76bf51d908eb7162745ea9b7c21fb9c2350d
Côme Perrot <cperrot@laas.fr>
2025-09-15 01:44:14 +0200

    Improve navigation layer

--- keyboards/redox/keymaps/ComePerrot/keymap.c
+++ keyboards/redox/keymaps/ComePerrot/keymap.c
@@ -5,8 +5,10 @@ enum layers { _BASE, _FUNC, _NAV };
 enum my_keycodes { RGB_SWT = SAFE_RANGE, LAYER_RST };
 
 // Shortcut to make keymap more readable
+#define FUNC_SPC LT(1, KC_SPC)
 #define FUNC_MO MO(_FUNC)
 #define FUNC TG(_FUNC)
+#define NAV_ESC LT(2, KC_ESC)
 #define NAV_MO MO(_NAV)
 #define NAV TG(_NAV)
 #define COLON RSFT(KC_N)
@@ -19,11 +21,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB  ,KC_Q    ,KC_W    ,KC_E    ,KC_R    ,KC_T    ,KC_NO   ,                          KC_NO   ,KC_Y    ,KC_U    ,KC_I    ,KC_O    ,KC_P    ,FUNC_MO ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_ESC  ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_NO   ,                          KC_NO   ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,COLON   ,
+     NAV_ESC ,KC_A    ,KC_S    ,KC_D    ,KC_F    ,KC_G    ,KC_NO   ,                          KC_NO   ,KC_H    ,KC_J    ,KC_K    ,KC_L    ,KC_SCLN ,COLON   ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT ,KC_Z    ,KC_X    ,KC_C    ,KC_V    ,KC_B    ,FUNC    ,NAV     ,        NAV     ,FUNC    ,KC_N    ,KC_M    ,KC_COMM ,KC_DOT  ,KC_SLSH ,KC_RSFT ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
-     KC_LALT ,KC_LGUI ,NAV_MO  ,FUNC_MO ,     KC_LCTL ,    KC_SPC  ,KC_ENT  ,        KC_DEL  ,KC_BSPC ,    KC_RALT ,     KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT 
+     KC_LALT ,KC_LGUI ,NAV_MO  ,FUNC_MO ,     KC_LCTL ,    FUNC_SPC,KC_ENT  ,        KC_DEL  ,KC_BSPC ,    KC_RALT ,     KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RGHT 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
   ),
 
@@ -31,11 +33,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                                           ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_TRNS ,KC_F1   ,KC_F2   ,KC_F3   ,KC_F4   ,KC_F5   ,                                            KC_F6   ,KC_F7   ,KC_F8   ,KC_F9   ,KC_F10  ,KC_TRNS ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐                         ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRNS ,KC_MPRV ,KC_MPLY ,KC_MNXT ,KC_NO   ,KC_NO   ,KC_F11  ,                          KC_F12  ,KC_NO   ,KC_END  ,KC_HOME ,KC_NO   ,KC_NO   ,KC_TRNS ,
+     KC_TRNS ,KC_MPRV ,KC_MPLY ,KC_MNXT ,KC_NO   ,KC_NO   ,KC_F11  ,                          KC_F12  ,KC_HOME ,KC_PGDN ,KC_PGUP ,KC_END  ,KC_NO   ,KC_TRNS ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┤                         ├────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TRNS ,KC_MUTE ,KC_VOLD ,KC_VOLU ,KC_NO   ,KC_NO   ,KC_NO   ,                          KC_NO   ,KC_LEFT ,KC_DOWN ,KC_UP   ,KC_RIGHT,KC_NO   ,KC_NO   ,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┐       ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_TRNS ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,RGB_SWT ,KC_TRNS ,KC_TRNS ,        KC_TRNS ,KC_TRNS ,KC_NO   ,KC_PGDN ,KC_PGUP ,KC_NO   ,KC_NO   ,KC_TRNS ,
+     KC_TRNS ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,RGB_SWT ,KC_TRNS ,KC_TRNS ,        KC_TRNS ,KC_TRNS ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_NO   ,KC_TRNS ,
   //├────────┼────────┼────────┼────────┼────┬───┴────┬───┼────────┼────────┤       ├────────┼────────┼───┬────┴───┬────┼────────┼────────┼────────┼────────┤
      KC_TRNS ,KC_NO   ,KC_TRNS ,KC_TRNS ,     KC_TRNS ,    KC_TRNS ,KC_TRNS ,        KC_TRNS ,KC_TRNS ,    KC_TRNS ,     KC_TRNS ,KC_TRNS ,KC_TRNS ,KC_TRNS 
   //└────────┴────────┴────────┴────────┘    └────────┘   └────────┴────────┘       └────────┴────────┘   └────────┘    └────────┴────────┴────────┴────────┘
